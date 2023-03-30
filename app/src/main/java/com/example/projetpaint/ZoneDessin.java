ece0d5107cd0de4f319a2f09df7249ce5fde3da6
lesguardian <jules.guesdon@etu.univ-lehavre.fr>
2023-03-30 15:30:22 +0200

    Essaie pour dessiner(pas reussi)

--- app/src/main/java/com/example/projetpaint/ZoneDessin.java
+++ app/src/main/java/com/example/projetpaint/ZoneDessin.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
 
-public class ZoneDessin extends View {
+public class ZoneDessin extends View implements View.OnTouchListener {
 
     private ArrayList<IForme> formes = new ArrayList<IForme>();
     private Paint paint = new Paint();
@@ -16,6 +17,7 @@ public class ZoneDessin extends View {
     public ZoneDessin(Context context) {
         super(context);
         paint.setStrokeWidth(10);
+        setOnTouchListener(this);
     }
 
     public void onDraw(Canvas canvas){
@@ -23,5 +25,25 @@ public class ZoneDessin extends View {
         for (IForme forme : formes){
             forme.dessiner(canvas, paint);
         }
+    }
+
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+            formes.add(new IForme() {
+                @Override
+                public ConstanteCommune.TypeDeFormes getForme() {
+                    return null;
+                }
+
+                @Override
+                public void dessiner(Canvas canvas, Paint paint) {
+
+                }
+            });
+            return true;
+        }
+        return false;
     }
 }
